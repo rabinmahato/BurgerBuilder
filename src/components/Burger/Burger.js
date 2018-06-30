0b09969ec3873aaa81b991c1f66f850908bca80c
Rabin Mahato <reach.rabin.mahato@gmail.com>
2018-06-30 15:34:59 +0530

    state added for ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,12 +2,27 @@ import React from 'react';
 import BurgerIngredients from './BurgerIngredients/BurgerIngredients';
 import classes from './Burger.css';
 
-const burger = () => {
+const burger = (props) => {
+    let transformedIngredients = Object.keys(props.ingredients)
+    .map((ingredientKey) =>{
+        /* Create <BurgerIngredients/> component */
+        return [...Array(props.ingredients[ingredientKey])].map((_, index)=>{ /* [ , , ] if return 3 */
+            return <BurgerIngredients key={ ingredientKey + index } type={ ingredientKey }/>
+        }); 
+    })
+    /* reduce the array with only ingredients */
+    .reduce((arr, element) => {
+        return arr.concat(element)
+    },  []);
+
+    if(transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding the Ingredients</p>;
+    }
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredients type='bread-top'/>
-            <BurgerIngredients type='cheese'/>            
-            <BurgerIngredients type='meat'/>
+                { transformedIngredients }
             <BurgerIngredients type='bread-bottom'/>
         </div>
     );
--- src/container/BurgerBuilder/BurgerBuilder.js
+++ src/container/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,19 @@ import AuxBox from '../../hoc/AuxBox';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 3,
+            cheese: 5,
+            meat: 2
+        }
+    }
     render() {
         return(
             <AuxBox>
-                <Burger/>
+                <Burger ingredients={this.state.ingredients}>
+                </Burger>
                 <div> Build Controls</div>
             </AuxBox>
         );
